cd6ac7600148dbf4ebd5b7c5e1b1093fd17c06c0
jyotikapoor01832 <jyotikapoor01832@gmail.com>
2024-01-28 08:21:19 +0530

    check number is palindrom or not

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+num=int(input("Enter any number:"))
+number=num
+ans=0
+
+while num >0:
+    rem = num %10
+    ans=ans*10 + rem
+    num=num//10
+print(ans)
+print(number)
+
+if number==ans:
+    print(ans," is palindrom")
+else:
+    print(ans,"is not palindrom")
