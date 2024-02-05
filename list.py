b55f02f6f42355a490613e92898099bb68751c83
jyotikapoor01832 <jyotikapoor01832@gmail.com>
2024-02-05 21:22:04 +0530

    sum and average of elements within a list

---
+++ list.py
@@ -0,0 +1,7 @@
+lst=[10,20,30,40,50]
+count=sum(lst)
+avg=count/len(lst)
+print( "list:",lst)
+print("sum of all the numbers in a list=",count)
+avg=count/len(lst)
+print("average of all numbers in a list=",avg)
