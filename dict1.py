1c5c8830d2bd7af8d050099b1de92e59dbf6c3fc
jyotikapoor01832 <jyotikapoor01832@gmail.com>
2024-05-20 22:15:06 +0530

    Update

--- dict1.py
+++ dict1.py
@@ -3,3 +3,4 @@ country_capital={"India":"New Delhi",
                  "England":"London"}
 print(country_capital);
 print(len(country_capital))         #Dictionary length
+print(country_capital["India"])     #Access dictionary item
