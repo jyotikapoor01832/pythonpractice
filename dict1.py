a2983920e3f5604c894baa0ead8b1f396391ee85
jyotikapoor01832 <jyotikapoor01832@gmail.com>
2024-05-22 21:57:14 +0530

    Update

--- dict1.py
+++ dict1.py
@@ -2,7 +2,7 @@ country_capital={"India":"New Delhi",
                  "China":"Beiging",
                  "England":"London"}
 print(country_capital);
-print(len(country_capital))         #Dictionary length
+print("Length : ",len(country_capital))         #Dictionary length
 print(country_capital["India"])     #Access dictionary item
 country_capital["Afghanistan"]="Kabul"    # Item added
 print("New dictionary", country_capital);
