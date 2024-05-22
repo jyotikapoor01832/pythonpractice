cf471e65ef969121e2e17f3a1cbb5955df337487
jyotikapoor01832 <jyotikapoor01832@gmail.com>
2024-05-22 21:54:50 +0530

    New addition

--- dict1.py
+++ dict1.py
@@ -4,3 +4,5 @@ country_capital={"India":"New Delhi",
 print(country_capital);
 print(len(country_capital))         #Dictionary length
 print(country_capital["India"])     #Access dictionary item
+country_capital["Afghanistan"]="Kabul"    # Item added
+print("New dictionary", country_capital);
